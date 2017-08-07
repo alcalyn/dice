ecbf0cdf539ce8bda165d477f832163751908d35
Julien Maulny <doubjulien@hotmail.fr>
2017-08-07 13:15:49 +0200

    remove testing face

--- dice/dice.py
+++ dice/dice.py
@@ -1,14 +1,13 @@
 from chall_cube.Cube import Cube
 from chall_cube.device.Accelerometer import Accelerometer
 from chall_cube.face.BlankFace import BlankFace
-from chall_cube.face.BlockingFace import BlockingFace
 from face.gravity_path_face.GravityPathFace import GravityPathFace
 
 accelerometer = Accelerometer()
 
 faces = [
     BlankFace(),
-    BlockingFace(),
+    BlankFace(),
     BlankFace(),
     BlankFace(),
     GravityPathFace(accelerometer),
